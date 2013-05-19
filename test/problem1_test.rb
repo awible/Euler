5dd21756af217851680f692ecfc1575b0d1fd27c
awible <apwible@gmail.com>
2013-05-19 01:36:15 -0400

    Move rspec and tests to their own directories

---
+++ test/problem1_test.rb
@@ -0,0 +1,4 @@
+require 'problem1'
+multiples_of_n_up_to(3,5) {|n| puts n}
+puts sum_multiples_of_n_up_to(3,5)
+puts sum_multiples_of_n_up_to(3,9) + sum_multiples_of_n_up_to(5,9)
---
+++ test/problem2_test.rb
@@ -0,0 +1,4 @@
+require 'problem2'
+puts even_fib_up_to(13) {|n| puts n}
+puts sum_even_fib_up_to(13)
+puts sum_even_fib_up_to(3999999)
