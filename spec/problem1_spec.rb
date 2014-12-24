ed6615a2ff592565a7d2c5a0ae0c23f89fe0799d
Adrian Wible <apwible@gmail.com>
2014-12-24 10:56:55 -0500

    change to rspec expect syntax

--- spec/problem1_spec.rb
+++ spec/problem1_spec.rb
@@ -1,12 +1,12 @@
 require './problem1'
 describe "sum_multiples_of_n_up_to" do
   it "returns_3_for_multiples_of_3_up_to_5" do
-    sum_multiples_of_n_up_to(3,5).should eq(3)
+    expect(sum_multiples_of_n_up_to(3,5)).to eq(3)
   end
   it "returns_18_for_multiples_of_3_up_to_9" do
-    sum_multiples_of_n_up_to(3,9).should eq(18)
+    expect(sum_multiples_of_n_up_to(3,9)).to eq(18)
   end
   it "returns_233168_for_multiples_of_3_and_5_up_to_9" do
-    (sum_multiples_of_n_up_to(3,999)+sum_multiples_of_n_up_to(5,999)-sum_multiples_of_n_up_to(15,999)).should eq(233168)
+    expect(sum_multiples_of_n_up_to(3,999)+sum_multiples_of_n_up_to(5,999)-sum_multiples_of_n_up_to(15,999)).to eq(233168)
   end
 end
--- spec/problem2_spec.rb
+++ spec/problem2_spec.rb
@@ -4,18 +4,18 @@ describe "even_fib_up_to_12" do
     array_2_8 = [2,8]
     myarray = []
     even_fib_up_to(12) {|n| myarray.push(n)}
-    myarray.should eq(array_2_8)
+    expect(myarray).to eq(array_2_8)
   end
 end
 
 describe "sum_even_fib_up_to" do
   it "returns_10_for_even_fib_up_to_9" do
-    sum_even_fib_up_to(9).should eq(10)
+    expect(sum_even_fib_up_to(9)).to eq(10)
   end
   it "returns_44_for_even_fib_up_to_46" do
-    sum_even_fib_up_to(46).should eq(44)
+    expect(sum_even_fib_up_to(46)).to eq(44)
   end
   it "returns_4,613,732_for_even_fib_up_to_3999999" do
-    sum_even_fib_up_to(3999999).should eq(4613732)
+    expect(sum_even_fib_up_to(3999999)).to eq(4613732)
   end
 end
