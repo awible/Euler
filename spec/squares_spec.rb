6f28706c0d2ab9bd2678bf4ccd28dd8a38b433b5
Adrian Wible <apwible@gmail.com>
2015-10-12 12:55:59 -0400

    Fix spec name

--- spec/squares_spec.rb
+++ spec/squares_spec.rb
@@ -10,4 +10,13 @@ describe "squares" do
   it "returns_5_for_grid_2_2" do
     expect(squares_in(2,2)).to eq(5)
   end
+  it "returns_14_for_grid_3_3" do
+    expect(squares_in(3,3)).to eq(14)
+  end
+  it "returns_20_for_grid_3_4" do
+    expect(squares_in(3,4)).to eq(20)
+  end
+  it "returns_same_for_xy_as_yx" do
+    expect(squares_in(3,4)).to eq(squares_in(4,3))
+  end
 end
--- squares.rb
+++ squares.rb
@@ -3,11 +3,12 @@
 
 def squares_in(x,y)
   case
+    when (x==0 or y==0) then return 0
     when (x==1 and y==1) then return 1 
-    when (x==1 and y==2) then return 2
-    when (x==2 and y==2) then return 5 
-    when (x==3 and y==3) then return 14 
+    # when (x==1 and y==2) then return 2
+    # when (x==2 and y==2) then return 5 
+    # when (x==3 and y==3) then return 14 
   else
-    0
+    return x*y+squares_in(x-1,y-1) 
   end
 end
